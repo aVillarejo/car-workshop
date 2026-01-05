1ac093bd0cf211a4e313166fe6e3bbb142778c54
Alberto Villarejo <ing.albertovillarejo@gmail.com>
2026-01-05 11:51:00 -0600

    chore: clean Vite boilerplate and setup base structure

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <h1 className="font-bold text-4xl text-center text-balance tracking-tight scroll-m-20">
+        Vite + React
+      </h1>
+    </div>
+  )
+}
+
+export default App
